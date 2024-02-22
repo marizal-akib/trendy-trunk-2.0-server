6a59ae57f0590a3879848ea196a11609e1c3ac11
marizal-akib <marizalakib@gmail.com>
2024-02-22 17:26:46 +0000

    add cart delete API

--- index.js
+++ index.js
@@ -178,7 +178,12 @@ app.get('/carts', async (req, res) => {
   res.send(result);
 })
 
-
+app.delete('/carts/:id', async (req, res) => {
+  const id = req.params.id
+  const query = { _id: new ObjectId(id) }
+  const result = await cartCollection.deleteOne(query);
+  res.send(result);
+})
 
 
     // Send a ping to confirm a successful connection
